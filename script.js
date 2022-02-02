621a89677edea56ee0875e7f06d79c0eab3b4f71
Yharai <yharai5cm@gmail.com>
2022-02-02 09:58:50 -0800

    your comment

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+	let status=["no","no","no","no","no","no","no"];
+	for(let n=1; n<=7; n++)
+	{
+		$("#j"+n).click (function() {
+			
+			if(status[n]=="no")
+	
+			{	
+				$("#u"+n).css("visibility", "visible");
+			
+			
+		status[n]="yes";
+			}
+	
+	else{
+		$("#u"+n).css("visibility", "hidden");
+		status[n]="no";
+	}
+			
+		});
+	}
+	
+	});
 
